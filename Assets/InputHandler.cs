9b675315d3828642e8dced57419616357f38753f
Demetri <dnekos2462@gmail.com>
2020-06-05 20:18:32 -0400

    Revamp now with 2 player
    Almost completely redid movement. used 2d composite to fix opposite direction weirdness, and used input managers to allow for multiple players

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    private Movement mover;
+    private PlayerInput input;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        input = GetComponent<PlayerInput>();
+        var index = input.playerIndex;
+        var movers = FindObjectsOfType<Movement>();
+        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+    }
+
+    // Update is called once per frame
+    public void OnMove(CallbackContext context)
+    {
+        if (mover != null)
+        mover.inputVector = context.ReadValue<Vector2>(); 
+    }
+}
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,60 +5,23 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    GamepadControls gcontrols;
-    KeyControls kcontrols;
-
-    public int controls = 3;// (0 keyboard 1 gp)
-
-    public Vector2 move;
     public float speed = 10f;
+
+    [SerializeField]
+    private int playerindex = 0;
+
+    public Vector2 inputVector;
+
     private void Awake()
     {
-        gcontrols = new GamepadControls();
-        gcontrols.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        gcontrols.Gameplay.Move.canceled += ctx => move = Vector2.zero;
-
-        kcontrols = new KeyControls();
-        kcontrols.Gameplay.MoveUp.performed += ctx => move.y = 1;
-        kcontrols.Gameplay.MoveUp.canceled += ctx => move.y = 0;
-
-        kcontrols.Gameplay.MoveDown.performed += ctx => move.y =-1;
-        kcontrols.Gameplay.MoveDown.canceled += ctx => move.y = 0;
-
-        kcontrols.Gameplay.MoveLeft.started += ctx => move.x = -1;
-        kcontrols.Gameplay.MoveLeft.canceled += ctx => move.x = 0;
-
-        kcontrols.Gameplay.MoveRight.started += ctx => move.x = 1;
-        kcontrols.Gameplay.MoveRight.canceled += ctx => move.x = 0;
     }
 
-
+    public int GetPlayerIndex()
+    {
+        return playerindex;
+    }
     private void Update()
     {
-        Vector2 m = move * Time.deltaTime * speed;
-        transform.Translate(m);
-
-        if (controls == 0)
-        {
-            gcontrols.Gameplay.Disable();
-            kcontrols.Gameplay.Enable();
-            //   move = Vector2.zero;
-        }
-        else if (controls == 1)
-        {
-            gcontrols.Gameplay.Enable();
-            kcontrols.Gameplay.Disable();
-        }
-    }
-
-    private void OnEnable()
-    {
-        gcontrols.Gameplay.Enable();
-        kcontrols.Gameplay.Enable();
-    }
-    private void OnDisable()
-    {
-        gcontrols.Gameplay.Disable();
-        kcontrols.Gameplay.Disable();
+        transform.Translate(inputVector*Time.deltaTime*speed);
     }
 }
