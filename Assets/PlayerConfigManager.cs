ead47699f7408c46f5e033fc88e725ef3ed81f66
Demetri <dnekos2462@gmail.com>
2020-06-24 16:56:20 -0400

    halfway through (mouse/select screen)
    got control schemes to accept mouse as part of the keyboard
    spent 3 hours working on player select screen, but not done yet
    fixed some misc stuff like merge errors

---
+++ Assets/PlayerConfigManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerConfigManager : MonoBehaviour
+{
+    private List<PlayerConfiguration> playerConfigs;
+    [SerializeField]
+    int maxPlayers = 4;
+
+    public static PlayerConfigManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.Log("trying to make new instances");
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            playerConfigs = new List<PlayerConfiguration>();
+        }
+    }
+
+    public void ReadyPlayer (int index)
+    {
+        playerConfigs[index].isReady = true;
+        if(playerConfigs.All(p => p.isReady == true))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+    public void HandlePlayerJoin(PlayerInput pi)
+    {
+        Debug.Log("Player " + pi.playerIndex + " Joined");
+        if(!playerConfigs.Any(p=>p.playerIndex == pi.playerIndex))
+        {
+            pi.transform.SetParent(transform);
+            playerConfigs.Add(new PlayerConfiguration(pi));
+        }
+    }
+}
+public class PlayerConfiguration
+{
+
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        playerIndex = pi.playerIndex;
+        theInput = pi;
+    }
+    public PlayerInput theInput { get; set; }
+    public int playerIndex { get; set; }
+    public bool isReady { get; set; }
+    
+}
---
+++ Assets/SetupMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetupMenuController : MonoBehaviour
+{
+    private int PlayerIndex;
+    [SerializeField]
+    Text titleText;
+    [SerializeField]
+    Button readyButton;
+
+    public void SetPlayerindex(int pi)
+    {
+        PlayerIndex = pi;
+        titleText.text = "Player " + (pi + 1);
+    }
+    public void ReadyPlayer()
+    {
+        PlayerConfigManager.Instance.ReadyPlayer(PlayerIndex);
+        readyButton.gameObject.SetActive(false);
+    }
+}
