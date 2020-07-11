354617bac2e34b4d3093b7d6fd878fb7c2272053
Demetri <dnekos2462@gmail.com>
2020-07-10 23:37:30 -0400

    New Movement
    a new movement system, in contrast to the tank controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,6 +14,10 @@ public enum StatusConditions
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField]
+    bool TankControls;
+
+    [HideInInspector]
     public Vector2 inputVector;
     [HideInInspector]
     public float grab = 0;
@@ -42,6 +46,12 @@ public class Movement : MonoBehaviour
     [HideInInspector]
     public Vector2 temp_translate = Vector2.zero;
 
+    Vector2 newspeed;
+    float newturnspeed;
+    float target_degree;
+    [SerializeField]
+    int reverse_allowance = 25;
+
     private void Start()
     {
         temp_drag = 1f;
@@ -50,28 +60,62 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.Log("drag: " + temp_drag+", speed"+temp_speed);
-        //gives the speed 
-        speed += inputVector.y * (acceleration * Time.deltaTime) * temp_speed;
+        if (TankControls)
+        {
+            //gives the speed 
+            speed += inputVector.y * (acceleration * Time.deltaTime) * temp_speed;
 
-        if (Mathf.Abs(speed) > maxSpeed)
-            speed = maxSpeed;
-        transform.Translate(Vector2.up * speed, Space.Self);
+            if (Mathf.Abs(speed) > maxSpeed)
+                speed = maxSpeed;
+            transform.Translate(Vector2.up * speed, Space.Self);
 
-        //gives it the drag
-        speed = speed * Drag * temp_drag;
-        if (Mathf.Abs(speed) <= minSpeed && inputVector.y == 0)
-            speed = 0;
+            //gives it the drag
+            speed = speed * Drag * temp_drag;
+            if (Mathf.Abs(speed) <= minSpeed && inputVector.y == 0)
+                speed = 0;
 
-        turnSpeed += inputVector.x * turnAcceleration * Time.deltaTime * temp_speed;
+            turnSpeed += inputVector.x * turnAcceleration * Time.deltaTime * temp_speed;
 
-        transform.Rotate(Vector3.back * turnSpeed);
-        turnSpeed *= Drag * temp_drag;
-        if (speed < 0 && turnSpeed < 0)
-            turnSpeed = Mathf.Abs(turnSpeed) * -1;
-        else if (speed < 0 && turnSpeed > 0)
-            turnSpeed = Mathf.Abs(turnSpeed);
-        if (Mathf.Abs(speed) <= minSpeed && inputVector.y == 0)
-            speed = 0;
+            transform.Rotate(Vector3.back * turnSpeed);
+            turnSpeed *= Drag * temp_drag;
+            if (speed < 0 && turnSpeed < 0)
+                turnSpeed = Mathf.Abs(turnSpeed) * -1;
+            else if (speed < 0 && turnSpeed > 0)
+                turnSpeed = Mathf.Abs(turnSpeed);
+            if (Mathf.Abs(speed) <= minSpeed && inputVector.y == 0)
+                speed = 0;
+        }
+        else
+        {
+            //gives the speed 
+            newspeed += inputVector * (acceleration * Time.deltaTime) * temp_speed;
+
+            Vector2.ClampMagnitude(newspeed, maxSpeed);
+
+            target_degree = (Mathf.Atan2(-newspeed.x, newspeed.y) * Mathf.Rad2Deg + 360) % 360;
+            bool fob = !(Mathf.DeltaAngle(transform.eulerAngles.z + 180,target_degree) < Mathf.Abs(reverse_allowance));
+            
+            if (fob)
+                transform.Translate(Vector2.up * newspeed.magnitude, Space.Self);
+            else
+                transform.Translate(Vector2.down * newspeed.magnitude, Space.Self);
+
+            //gives it the drag
+            newspeed = newspeed * Drag * temp_drag;
+
+            if (Mathf.Abs(speed) <= minSpeed && inputVector.y == 0)
+                speed = 0;
+
+            float angle;
+            if (fob)
+            {
+                newturnspeed += inputVector.magnitude * turnAcceleration * Time.deltaTime * temp_speed;
+                angle = Mathf.LerpAngle(transform.eulerAngles.z, target_degree, newturnspeed);
+            }
+            else
+                angle = Mathf.LerpAngle(transform.eulerAngles.z, target_degree + 180, newturnspeed);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            newturnspeed *= Drag * temp_drag;
+        }
     }
 }
